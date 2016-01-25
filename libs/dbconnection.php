393726be2bfef66697f50fa7f769fe7909b690e9
mraju2 <search4gyan@gmail.com>
2016-01-25 17:09:29 +0530

    Db Connection Class created.

--- libs/dbconnection.php
+++ libs/dbconnection.php
@@ -1,10 +1,67 @@
 <?php
 //DB Configuration for blinx server
-$database = "blix";
+$database = "test";
 $host = "localhost";
-$user = "root";
-$pass = "";
-
+$user = "TCSCS";
+$pass = "tsoltsip13a";
+class DB {
+	
+	protected $db_name = 'test';
+	protected $db_user = 'TCSCS';
+	protected $db_pass = 'tsoltsip13a';
+	protected $db_host = 'localhost';
+	
+	// Open a connect to the database.
+	// Make sure this is called on every page that needs to use the database.
+	
+	private function connect() {
+	
+		$mysqli = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+		if ($mysqli->connect_error) 
+		{
+			die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+		}
+		else
+		{
+			return $connect_db;
+		}
+		return '';
+	}
+	private function close($connection)
+	{
+		if(!mysqli_close($connection)){
+			echo "Connection close failed.";
+		}
+	}
+	public function runQuery($query) {
+	
+		$conn= $this -> connect();
+		if($conn!='')
+		{
+			$result = $conn -> query($query);
+			if (!$result) 
+			{
+				echo "Could not successfully run query ($sql_final) from DB: " . mysql_error();
+				exit;
+			}
+			else
+			{
+				$data = array();
+				while ($row = $result->fetch_array(MYSQLI_ASSOC)) 
+				{
+					array_push($data[], $row);
+				}
+				printf ("%s (%s)\n", $row[0], $row[1]);
+				$result->free();
+				$conn->close();
+				return $row[0];
+			}
+			return '';
+		}
+	}
+}
+?>
 
 
 
